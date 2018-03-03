0e0ddfc54eacd3130d39543d5644d3a6e375906b
Caroline Kaleel <cocokaleel@gmail.com>
2018-03-03 09:10:35 -0800

    Re-File dump because the last push was broken
    Addition of video elements

---
+++ scripts.js
@@ -0,0 +1,19 @@
+webgazer.begin()
+
+/* All the stuff that deals with video */
+var video = document.querySelector("#videoElement");
+ 
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+ 
+if (navigator.getUserMedia) {       
+    navigator.getUserMedia({video: true}, handleVideo, videoError);
+}
+ 
+function handleVideo(stream) {
+    video.src = window.URL.createObjectURL(stream);
+}
+ 
+function videoError(e) {
+    // do something
+}
+
